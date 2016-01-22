09d376833243849ef15fe6f77da60c771767d7c8
iamhadia <go@hadi.com.au>
2016-01-22 11:35:07 -0500

    inital
    auth ignored

---
+++ config/database.js
@@ -0,0 +1,5 @@
+module.exports = {
+
+    'url' : 'mongodb://hadi:password@ds039421.mongolab.com:39421/testdatabasehadi' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
